cf5374933b791232721c13e0c0a5d80a3c7ee68d
kishor_saha <kishorsaha650@gmail.com>
2022-04-15 03:23:19 +0600

    commit

--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,28 @@
 jQuery(function ($) {
     "use strict"
+    $('.toggle-submenu-show').on('click', function () {
+        $('.toggle-submenu').slideToggle();
+    });
+
+    $('.menu-tggle').on('click', function () {
+        $('.menu-toggle-bar').addClass('show');
+
+    });
+
+    $('.toggle-close-btn').on('click', function () {
+        $('.menu-toggle-bar').removeClass('show');
+    });
+
+
+
+
+    $('.menu-search').on('click', function () {
+        $('.search-panel').removeClass('popup-hide');
+    });
+
+    $('.btn-popup-close').on('click', function () {
+        $('.search-panel').addClass('popup-hide');
+    });
 
 
     $('.video-icon').magnificPopup({
